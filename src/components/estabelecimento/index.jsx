7d71e3a622eb33900e89f528e89b7606fa44bf3c
Miguel03706 <migueltjsilva@gmail.com>
2022-11-04 12:48:22 -0300

    exclusao de arquivos desnecessarios

---
+++ src/components/estabelecimento/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function estabelecimento() {
+    return (
+        <>
+            <p>
+                Teste
+            </p>
+            <p>
+                <b>AAAA</b>
+            </p>
+        </>
+    );
+}
+
+export default estabelecimento;
